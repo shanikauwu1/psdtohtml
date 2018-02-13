145bf01427361273132f3ce69ef344dfb61f058a
shanika Jayawardane <shanikauwu@gmail.com>
2018-02-13 14:04:09 +0400

    Add files via upload

---
+++ .gitignore/index.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <title>Hotel Deluxe</title>
+        <link href="css/style.css" rel="stylesheet" type="text/css">
+        <meta name="viewport" content="width=device-width">  
+       
+    </head>
+    
+    <body>
+        
+        <header>
+            <div class="wrapper">
+                
+              <nav>
+                    <div class="top-nav">
+                        <ul>
+                            <li><a href=""><img src="images/search.png" /></a></li>
+                            <li><a href="">Contact</a></li>
+                            <li><a href="">Enquiry</a></li>
+                          
+                        </ul>
+                    </div>
+                
+                
+                   
+                     <div class="container">
+                          <div class="logo"><img src="images/logo.png"></div>
+                          <a href="" class="item">Location</a>
+                          <a href="" class="item">Explore</a>
+                          <a href="" class="item">Life Style</a>
+                          <a href="" class="item">Discover</a>
+                          <a href="" class="item">Gallery</a>
+                          <a href="" class="item">About Us</a>
+                        </div>
+
+                
+                </nav>
+            
+        </header>
+
+          </div> 
+        
+        <div id="main-banner">
+
+            <div class="trans-div">
+                <ul>
+                    <li>Residential</li>
+                    <li>*</li>
+                    <li>Amenties</li>
+                    <li>*</li>
+                    <li>Offices</li>
+                    <li>*</li>
+                    <li>Show Rooms</li>
+                </ul>
+
+            </div>
+
+            <h2 > A Brilliant </br> in tne sky</h2>
+            <picture>
+                <source srcset="images/banner-s.jpg" media="(max-width: 480px)">
+                <source srcset="images/banner-m.jpg" media="(max-width: 768px)">
+                <img srcset="images/banner.jpg" alt="banner">
+            </picture> 
+        </div>
+        
+        
+        
+        
+            <section id="main-description">
+                
+               <h1>A diamond in the sky</h1>
+
+               <p>The tower's architecture boasts a linear design, virtually creating a diamond in the sky. A latticed exoskeleton elegantly cloaks the exterior, framing the views. Complimenting the glamorous exterior, the interiors convey a more subtle luxury through warm, natural finishes that exude sophistication</p>
+
+            </section>
+
+             <div id="director-description">
+                  <div class="side-left">
+                 <img src="images/director.jpg" >
+                 </div>
+                 <div class="side-right">
+                   <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley .</p>
+
+                   <h1>Creative Name and Lastname</h1>
+                   <h2>Director</h2>
+                 </div>
+
+            </div>
+
+             <div id="item-description">
+                  <div class="side-left">
+                 <img src="images/bed.jpg" class="bedroom-img">
+                
+                 </div>
+                 <div class="side-right">
+                    <img src="images/bath.jpg">
+                 </div>
+
+                 <div id='builidng-img'>
+                   <img src="images/buildingcutout.png" usemap="#image-map">
+
+                   <!-- <map name="image-map">
+                        <area target="" alt="" title="Offices" data-name="Offices</br> click to Explore" href="" coords="118,1186,378,1072" shape="rect">
+                        <area target="" alt="" title="Residential" data-name="Residential </br> click to Explore" href="" coords="318,400,88,504" shape="rect">
+                        <area target="" alt="" title="Amenties" data-name="Amenties </br> click to Explore " href="" coords="1149,757,1407,865" shape="rect">
+                        <area target="" alt="" title="showrooms" href="" data-name="showrooms </br> click to Explore" coords="1132,1340,1367,1244" shape="rect">
+                    </map>-->
+
+
+                 </div>
+            </div>
+
+            
+           
+        
+        <footer>
+            <button class="button button1">Download Brochure</button>
+            <button class="button button2">See Our fact sheet</button>
+        </footer>
+        
+    </body>
+    
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+    <script type="text/javascript" src="js/jquery.rwdImageMaps.js"></script>
+    <script src="js/script.js"></script>
+
+</html>
---
+++ .gitignore/js/script.js
@@ -0,0 +1,25 @@
+
+
+   $(document).ready(function(e) {
+   /* $('img[usemap]').rwdImageMaps();
+
+
+    $(function() {
+    
+    $('area').each(function(){
+        var txt=$(this).data('name');
+        var coor=$(this).attr('coords');
+        var coorA=coor.split(',');
+        var left=coorA[0];
+        var top=coorA[1];
+        
+        var $span=$('<span class="map_title">'+txt+'</span>');        
+        $span.css({top: top+'px', left: left+'px', position:'absolute'});
+        $span.appendTo('#builidng-img');
+    })
+   
+})*/ 
+//console
+});
+
+//log('xxxx');
